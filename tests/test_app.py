37dac1431f1f5bc0d33a6f04d5b06fa5cca0292d
elyas <elyashedayat1380@gmail.com>
2025-11-14 12:05:03 +0330

    Change: update install deps command

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,10 +13,3 @@ def test_health():
     assert data["status"] == "ok"
     assert "environment" in data
     assert "redis_url" in data
-
-
-def test_health_redis():
-    response = client.get("cache-test")
-    assert response.status_code == status.HTTP_200_OK
-    data = response.json()
-    assert data["stored_value"] == "Hello Redis!"
